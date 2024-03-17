9020fd74a7977b503b24933cd618ee184dd1d098
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-03-17 19:56:18 +0600

    connection to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 const port = 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
