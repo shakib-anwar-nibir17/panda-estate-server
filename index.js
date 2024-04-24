c070dba20235907cea88fecdc3452af8e6a21609
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-04-25 00:47:29 +0600

    api testing

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "*",
+    origin: "https://panda-estate-sn17.web.app",
     credentials: true,
   })
 );
